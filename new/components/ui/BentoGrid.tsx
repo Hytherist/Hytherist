8762ef9bad4168f563be27c91fe6e9b97806a6df
Derek Cheng <cyc.derek@gmail.com>
2024-11-08 23:17:47 -0800

    removed partial code

--- new/components/ui/BentoGrid.tsx
+++ new/components/ui/BentoGrid.tsx
@@ -70,9 +70,6 @@ export const BentoGridItem = ({
             />
           )}
         </div>
-        {id === 6 && (
-            
-        )
       </div>
       <div className="group-hover/bento:translate-x-2 transition duration-200">
         <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
